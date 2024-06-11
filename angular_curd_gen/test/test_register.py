005ab3c82dd4cf627cbc91ed96c9ae9952024b28
wz <zhou.wang@ydassess.com>
2024-06-11 14:47:25 +0800

    for updater

--- angular_curd_gen/test/test_register.py
+++ angular_curd_gen/test/test_register.py
@@ -29,8 +29,8 @@ class UsersAdmin(ModelAdmin):
     model_fields = ('id', 'username', 'lastname', 'firstname', 'gender', 'birthday', 'role', 'active', 'created')
     list_display_restraint = model_fields
     list_editable_restraint = ('username', 'gender')
-    model_edit_fields = ('username', 'lastname', 'firstname', 'gender', 'birthday', 'role', 'active')
-    model_create_fields = model_edit_fields
+    model_edit_fields = ('username', 'lastname', 'firstname', 'gender', 'birthday', 'active')
+    model_create_fields = ('username', 'lastname', 'firstname', 'gender', 'birthday', 'role', 'active')
     model_translate_fields = ('ID', '用户名', '姓氏', '名字', '性别', '生日', '角色', '是否激活', '创建时间')
 
 
