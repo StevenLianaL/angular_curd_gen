600da8bb961398cf77064c6c097e29f670562670
wz <zhou.wang@ydassess.com>
2024-06-18 10:11:35 +0800

    add rust mongo support

---
+++ angular_curd_gen/templates/rust/src/utils/db.rs
@@ -0,0 +1,21 @@
+use serde::Deserialize;
+use crate::config::DatabaseSettings;
+
+impl DatabaseSettings {
+    pub fn build_url(&self, db: &str) -> String {
+        format!("{}://{}:{}@{}:{}/{}", db, self.user, self.pswd, self.host, self.port, self.name)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RedisSettings {
+    pub host: String,
+    pub db: i32,
+    pub port: i32,
+}
+
+impl RedisSettings {
+    pub fn build_url(&self) -> String {
+        format!("redis://{}:{}/{}", self.host, self.port, self.db)
+    }
+}
